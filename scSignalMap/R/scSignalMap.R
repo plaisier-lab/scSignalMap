8b19a09896b4889efd33df685f783d0b1b3efa06
Christopher L Plaisier, PhD <cplaisier@systemsbiology.org>
2025-09-11 16:31:11 -0700

    New approach

--- scSignalMap/R/scSignalMap.R
+++ scSignalMap/R/scSignalMap.R
@@ -117,7 +117,11 @@ MapInteractions = function(seurat_obj, group_by, avg_log2FC_gte = 0.25, p_val_ad
     }
 
     # Cartesian product of LR pairs x cluster pairs
-    pairs_data = lr_dt[, cbind(.SD, clust_dt), by = seq_len(nrow(lr_dt))][, `:=`(seq_len, NULL)]
+    lr_dt[, dummy := 1]
+    clust_dt[, dummy := 1]
+
+    pairs_data <- lr_dt[clust_dt, on = "dummy", allow.cartesian = TRUE][, dummy := NULL]
+    #pairs_data = lr_dt[, cbind(.SD, clust_dt), by = seq_len(nrow(lr_dt))][, `:=`(seq_len, NULL)]
 
     cat(paste0('    Rows = ',nrow(pairs_data),'\n'))
 
