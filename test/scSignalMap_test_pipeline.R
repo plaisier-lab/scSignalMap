630aa9002b9fb6eeda7c8806c1a063f6403c3410
Jillian Woodley <j.s.woodley22@gmail.com>
2025-12-05 12:24:45 -0600

    Updated saving locations

--- test/scSignalMap_test_pipeline.R
+++ test/scSignalMap_test_pipeline.R
@@ -445,7 +445,7 @@ seurat1 = readRDS("seurat_objects/MN_big_int.rds")
 ## Run function and save as .csv (add gene_id='symbol' if needed)
 LR_interactions = MapInteractions(seurat1,
                                   'celltype')
-write.csv(LR_interactions, "MN_big_int_scSignalMap_interactions.csv")
+write.csv(LR_interactions, "scSignalMap_Testing/LR_interactions.csv")
 
 
 ## Find DE genes between MN2 and MN1 for GB3_Tumor cells
@@ -459,14 +459,14 @@ de_cond_celltype = find_markers_btwn_cond_for_celltype(
     celltype_name = "GB3_Tumor",
     FC_cutoff = 0.3,
     adj_p_val_cutoff = 0.05)
-write.csv(de_cond_celltype, "de_btwn_MN2_vs_MN1_GB3_Tumor.csv", row.names = FALSE)
+write.csv(de_cond_celltype, "scSignalMap_Testing/de_genes.csv", row.names = FALSE)
 
 
 ## Find upregulated receptors
 upreg_receptors = find_upreg_receptors(
     de_condition_filtered = de_cond_celltype,
     FC_cutoff = 0.3)
-write.csv(upreg_receptors, "upregulated_receptors_btwn_MN2_vs_MN1_GB3_Tumor.csv", row.names = FALSE)
+write.csv(upreg_receptors, "scSignalMap_Testing/upreg_receptors.csv", row.names = FALSE)
 
 ## Filter LR Interactions
 interactions_filtered = filter_lr_interactions(
@@ -480,7 +480,7 @@ interactions_filtered = filter_lr_interactions(
 upreg_receptors_filtered_and_compared = intersect_upreg_receptors_with_lr_interactions(
     upreg_receptors = upreg_receptors,
     interactions = interactions_filtered)
-write.csv(upreg_receptors_filtered_and_compared, "upreg_receptors_filtered_and_compared.csv", row.names = FALSE)
+write.csv(upreg_receptors_filtered_and_compared, "scSignalMap_Testing/upreg_receptors_filtered_and_compared.csv", row.names = FALSE)
 
 ## Find pathways that are enriched with DE genes that include the upregulated receptor
 enrichr_results = find_enriched_pathways(
@@ -489,7 +489,7 @@ enrichr_results = find_enriched_pathways(
     enrichr_databases = c("BioCarta_2016", "GO_Biological_Process_2025", "KEGG_2021_Human", "NCI-Nature_2016", "WikiPathways_2024_Human"),
     adj_p_val_method = "BH",
     adj_p_val_cutoff = 0.05)
-write.csv(enrichr_results, "enrichr_results.csv", row.names = FALSE)
+write.csv(enrichr_results, "scSignalMap_Testing/enrichr_results.csv", row.names = FALSE)
 
 
 ## Master List Call 
@@ -497,5 +497,5 @@ master_interaction_list = create_master_interaction_list(
   enrichr_results = enrichr_results,
   de_receptors = upreg_receptors_filtered_and_compared,
   scSignalMap_data_filtered = interactions_filtered)
-write.csv(master_interaction_list, "12.02.25_master_interaction_list.csv", row.names = FALSE)
+write.csv(master_interaction_list, "scSignalMap_Testing/12.05.25_master_interaction_list.csv", row.names = FALSE)
 
