b471a2d9672f3c1e2dbe9a0e1be0708e14f4382a
Christopher L Plaisier, PhD <plaisier@asu.edu>
2025-12-08 09:49:23 -0700

    Receptors not ligands

--- scSignalMap/R/scSignalMap.R
+++ scSignalMap/R/scSignalMap.R
@@ -231,8 +231,8 @@ find_upreg_receptors = function(de_condition_filtered= NULL, FC_cutoff = 0.3, sp
     message("Loading ligand-receptor information")
     # Load MultiNicheNet ligand receptor interactions
     lr_network = read.csv(system.file('extdata', 'lr_network.csv', package='scSignalMap'), header=TRUE)
-    receptor_ensembl = lr_network[,paste('ligand',species,'ensembl',sep='_')]
-    receptor_symbol = lr_network[,paste('ligand',species,'symbol',sep='_')]
+    receptor_ensembl = lr_network[,paste('receptor',species,'ensembl',sep='_')]
+    receptor_symbol = lr_network[,paste('receptor',species,'symbol',sep='_')]
     receptor_genes = unique(na.omit(receptor_ensembl))
     ensembl_to_symbol = setNames(receptor_symbol, receptor_ensembl)
 
