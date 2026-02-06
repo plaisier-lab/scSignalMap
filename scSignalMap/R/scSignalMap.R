fddc964b87d73db4de5b25793670e1b031bf1530
Jillian Woodley <j.s.woodley22@gmail.com>
2026-02-06 13:31:57 -0600

    Print clust1

--- scSignalMap/R/scSignalMap.R
+++ scSignalMap/R/scSignalMap.R
@@ -58,6 +58,7 @@ MapInteractions = function(seurat_obj, group_by, cond_column, cond_name1, avg_lo
     putative_markers = FindAllMarkers(seurat_obj, group.by=group_by, min.pct=min_pct, verbose=T)
     markers = list()
     for(clust1 in as.character(sort(unique(seurat_obj@meta.data[,group_by])))) {
+        print(clust1)
         markers[[clust1]] = putative_markers %>% dplyr::filter(cluster==clust1 & avg_log2FC>=avg_log2FC_gte & p_val_adj<=p_val_adj_lte) %>% dplyr::pull(name='gene')
         #cat(paste0('    ',cluster1,': ',length(markers[[cluster1]]),'\n'))
     }
